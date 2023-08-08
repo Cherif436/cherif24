1e98fecee638f025b70fb17cd1af5ced38ace938
Cherif436 <66637230+Cherif436@users.noreply.github.com>
2023-08-08 20:29:14 +0200

    Update projects.tsx

--- components/projects.tsx
+++ components/projects.tsx
@@ -20,7 +20,7 @@ export default function Projects() {
             <Project {...project} />
           </React.Fragment>
         ))}
-        <button className="px-6 py-3 mb-1 mr-1 text-sm font-bold text-white uppercase transition-all duration-150 ease-linear rounded-full shadow outline-none bg-sky-800 group hover:bg-sky-950 dark:bg-gray-900 bg-gradient-to-r active:bg-sky-900 hover:shadow-lg focus:outline-none" type="button" onClick={() => setArticleNum(articleNum + 2)}><em className='animate-pulse'>Voir plus</em></button>
+        <button className="px-6 py-3 mb-1 mr-1 text-sm font-bold text-white uppercase transition-all duration-150 ease-linear rounded-full shadow outline-none bg-sky-800 group hover:bg-sky-950 dark:bg-gray-900 bg-gradient-to-r active:bg-sky-900 hover:shadow-lg focus:outline-none" type="button" onClick={() => setArticleNum(articleNum + 2)}><em className='animate-pulse'>Show more</em></button>
       </div>
       
     </section>
