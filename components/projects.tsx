da004d0adc70a3157c954560e8247812b3be02db
Cherif436 <66637230+Cherif436@users.noreply.github.com>
2023-08-15 18:36:48 +0200

    Update projects.tsx

--- components/projects.tsx
+++ components/projects.tsx
@@ -9,18 +9,27 @@ import { useSectionInView } from '@/lib/hooks'
 export default function Projects() {
   const { ref } = useSectionInView("Projects", 0.5);
   const [articleNum, setArticleNum] = useState(4);
+  const [portfolios, setPortfolios] = useState(projectsData);
+
+  const loadMoreHandler = () => {
+    setArticleNum(prev => prev + 2);
+  }
 
   return (
     <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
       <SectionHeading>My projects</SectionHeading>
       
       <div>
-        {projectsData.slice(0, articleNum).map((project, index) => (
+        {portfolios?.slice(0, articleNum).map((project, index) => (
           <React.Fragment key={index}>
             <Project {...project} />
           </React.Fragment>
         ))}
-        <button className="px-6 py-3 mb-1 mr-1 text-sm font-bold text-white uppercase transition-all duration-150 ease-linear rounded-full shadow outline-none bg-sky-800 group hover:bg-sky-950 dark:bg-gray-900 bg-gradient-to-r active:bg-sky-900 hover:shadow-lg focus:outline-none" type="button" onClick={() => setArticleNum(articleNum + 2)}><em className='animate-pulse'>Show more</em></button>
+        {
+          articleNum < portfolios.length && projectsData.length > 4 && (
+            <button className="px-6 py-3 mb-1 mr-1 text-sm font-bold text-white uppercase transition-all duration-150 ease-linear rounded-full shadow outline-none bg-sky-800 group hover:bg-sky-950 dark:bg-gray-900 bg-gradient-to-r active:bg-sky-900 hover:shadow-lg focus:outline-none" type="button" onClick={loadMoreHandler}><em className='animate-pulse'>Load more</em></button>
+           )
+        }
       </div>
       
     </section>
