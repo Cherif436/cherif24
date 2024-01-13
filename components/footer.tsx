bebc42cac17f5acff17a64641bc3e9c39c3ec0b0
Cherif436 <66637230+Cherif436@users.noreply.github.com>
2024-01-13 10:46:38 +0100

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="px-4 mb-10 text-center text-gray-500">
       <small className="block mb-2 text-xs font-bold">
-        &copy; 2023 Cherif MERZOUD.
+        &copy; {new Date().getFullYear()} Cherif MERZOUD.
       </small>
       {/* <p className="text-xs">
         <span className="font-semibold">Site réalisé avec React & Next.js, TypeScript, Tailwind CSS,
